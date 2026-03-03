3eec0fb82693220f5d10fd7bf7e8e0c59f1e819e
sultanaljamaie-collab <sultan_aljamaie@hotmail.com>
2026-03-03 05:12:17 +0300

    Update channel.py

--- handlers/channel.py
+++ handlers/channel.py
@@ -1,1 +1,9 @@
+from aiogram import types
+from aiogram.dispatcher import Dispatcher
 
+async def channel_handler(message: types.Message):
+    if message.text == "📢 دخول القناة":
+        await message.reply("انضم للقناة 👇\nhttps://t.me/DealStorm_KSA")
+
+def register_handlers(dp: Dispatcher):
+    dp.register_message_handler(channel_handler)
