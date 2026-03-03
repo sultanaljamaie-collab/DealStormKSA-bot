a7b09bd733326bf4240e9ae30ecb1cae726b1d42
sultanaljamaie-collab <sultan_aljamaie@hotmail.com>
2026-03-03 05:11:02 +0300

    Update main_menu.py

--- keyboards/main_menu.py
+++ keyboards/main_menu.py
@@ -1,1 +1,8 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
+def main_keyboard():
+    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(KeyboardButton("🔥 أحدث الصفقات"))
+    keyboard.add(KeyboardButton("📢 دخول القناة"))
+    keyboard.add(KeyboardButton("💎 VIP قريبًا"))
+    return keyboard
