92132cdfdc8c094af472c0bcf7eeeed6ab73b9e9
sultanaljamaie-collab <sultan_aljamaie@hotmail.com>
2026-03-03 06:26:05 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,30 @@
-from handlers.start import register_start
-from admin import register_admin
 import logging
-from aiogram import Bot, Dispatcher
-from aiogram.utils import executor
-from config import BOT_TOKEN
+import os
 
-from handlers import start, channel
+from aiogram import Bot, Dispatcher, executor
 
+# استدعاء التسجيلات
+from handlers.start import register_start
+from handlers.channel import register_channel
+from admin import register_admin
+
+# تحميل التوكن من متغير البيئة
+API_TOKEN = os.getenv("BOT_TOKEN")
+
+# تفعيل اللوج
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token=BOT_TOKEN)
+# إنشاء البوت
+bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
-# تسجيل الهاندلرز
-start.register_handlers(dp)
-channel.register_handlers(dp)
+
+def register_handlers():
+    register_start(dp)
+    register_channel(dp)
+    register_admin(dp)
+
 
 if __name__ == "__main__":
+    register_handlers()
     executor.start_polling(dp, skip_updates=True)
