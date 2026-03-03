a71235206f49705fee54e4551de93ab52b09069f
sultanaljamaie-collab <sultan_aljamaie@hotmail.com>
2026-03-03 06:14:25 +0300

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,15 +1,18 @@
 from aiogram import types
 from aiogram.dispatcher import Dispatcher
+from database import add_user
 from keyboards.main_menu import main_keyboard
 
-async def start_handler(message: types.Message):
-    await message.reply(
+async def start_command(message: types.Message):
+    add_user(message.from_user.id)
+
+    await message.answer(
         "👋 أهلاً بك في DealStorm KSA\n\n"
         "🔥 أقوى الصفقات اليومية في السعودية\n"
         "💰 نوفر عليك البحث ونجيب لك الأفضل\n\n"
         "اختر من القائمة 👇",
-        reply_markup=main_keyboard()
+        reply_markup=main_keyboard
     )
 
-def register_handlers(dp: Dispatcher):
-    dp.register_message_handler(start_handler, commands=["start"])
+def register_start(dp: Dispatcher):
+    dp.register_message_handler(start_command, commands=["start"])
